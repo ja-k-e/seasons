5f2571e675abd50d09210d5d634d5fe53dc9b1c8
Jake Albaugh <jake.albaugh@gmail.com>
2021-09-10 22:02:51 +0000

    giving it a go

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const figures = document.querySelectorAll("figure");
 const hue = Math.round(Math.random() * 360);
 figures.forEach((figure, i) => {
-  figure.style.setProperty("--hue", i * 38 + hue);
+  figure.style.setProperty("--hue", hue + i * 105);
   const img = figure.querySelector("img");
   if (img.complete || img.readyState === 4) {
     figure.classList.add("loaded");
