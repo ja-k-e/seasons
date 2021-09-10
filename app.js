8da69348b1a9a304076048557f4bf2156b6628cb
Jake Albaugh <jake.albaugh@gmail.com>
2021-09-10 20:51:11 +0000

    giving it a go

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const images = document.querySelectorAll("div");
+const figures = document.querySelectorAll("figure");
 const hue = Math.round(Math.random() * 360);
-images.forEach((div, i) => {
-  div.style.setProperty("--hue", i * 38 + hue);
-  const img = div.querySelector("img");
+figures.forEach((figure, i) => {
+  figure.style.setProperty("--hue", i * 38 + hue);
+  const img = figure.querySelector("img");
   if (img.complete || img.readyState === 4) {
-    div.classList.add("loaded");
+    figure.classList.add("loaded");
   } else {
-    img.addEventListener("load", () => div.classList.add("loaded"));
+    img.addEventListener("load", () => figure.classList.add("loaded"));
   }
 });
